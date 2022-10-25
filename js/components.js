e30c685dad26c2fa96e531965d53d921ab58f0ec
EhtishamNazir <ehtishamnazir28@gmail.com>
2022-10-25 11:37:23 +0500

    Almost Done

--- js/components.js
+++ js/components.js
@@ -79,46 +79,17 @@ function closeAllSelect(elmnt) {
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
 
+
 /*---------------Dropdown Button Script----------------*/
 // Left sidebar dropdown content script that is next to search bar 
 function showDropdownContent() {
-  $(".dropdown-content").click(function() {
-    // body...
-  })
-  // var x = document.getElementById("dropdown-content");
-  // if (x.style.display === "none") {
-  //   x.style.display = "block";
-  // } else {
-  //   x.style.display = "none";
-  // }
+  var x = document.getElementById("dropdown-content");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
-
-/*---------------Layers Hide/Show Script---------------*/
-const accordionItemHeaders = document.querySelectorAll(".accordion");
-
-  accordionItemHeaders.forEach(accordionItemHeader => {
-     accordionItemHeader.addEventListener("click", event => {
-      
-       // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-      
-      const currentlyActiveAccordionItemHeader = document.querySelector(".accordion.active");
-      if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-         currentlyActiveAccordionItemHeader.classList.toggle("active");
-         currentlyActiveAccordionItemHeader.nextElementSibling.classList.remove ("active");
-       }
-
-       accordionItemHeader.classList.toggle("active");
-       const panel = accordionItemHeader.nextElementSibling;
-       if(accordionItemHeader.classList.contains("active")) {
-        panel.classList.add("active")
-       }
-       else {
-         panel.classList.remove("active")
-       }
-      
-     });
-  });  
-
 
 /*-------------Range Slider Script----------------*/
 // First let's set the colors of our sliders
@@ -151,119 +122,4 @@ function applyFill(slider) {
  // Our background color will change here
  const bg = `linear-gradient(90deg, ${settings.fill} ${percentage}%, ${settings.background} ${percentage+0.1}%)`;
  slider.style.background = bg;
-}
-
-/*------------------Filters Script-----------------*/
-function defer(method) {
-  if (window.jQuery)
-    method();
-  else
-    setTimeout(function() {
-      defer(method)
-    }, 50);
-}
-defer(function() {
-  (function($) {
-    
-    function doneResizing() {
-      var totalScroll = $('.filter-slider-frame').scrollLeft();
-      var itemWidth = $('.filter-slider-item').width();
-      var difference = totalScroll % itemWidth;
-      if ( difference !== 0 ) {
-        $('.filter-slider-frame').animate({
-          scrollLeft: '-=' + difference
-        }, 500, function() {
-          // check arrows
-          checkArrows();
-        });
-      }
-    }
-    
-    function checkArrows() {
-      var totalWidth = $('#filter-slider .filter-slider-item').length * $('.filter-slider-item').width();
-      var frameWidth = $('.filter-slider-frame').width();
-      var itemWidth = $('.filter-slider-item').width();
-      var totalScroll = $('.filter-slider-frame').scrollLeft();
-      
-      if ( ((totalWidth - frameWidth) - totalScroll) < itemWidth ) {
-        $(".next").css("visibility", "hidden");
-      }
-      else {
-        $(".next").css("visibility", "visible");
-      }
-      if ( totalScroll < itemWidth ) {
-        $(".prev").css("visibility", "hidden");
-      }
-      else {
-        $(".prev").css("visibility", "visible");
-      }
-    }
-    
-    $('.arrow').on('click', function() {
-      var $this = $(this),
-        width = $('.filter-slider-item').width(),
-        speed = 500;
-      if ($this.hasClass('prev')) {
-        $('.filter-slider-frame').animate({
-          scrollLeft: '-=' + width
-        }, speed, function() {
-          // check arrows
-          checkArrows();
-        });
-      } else if ($this.hasClass('next')) {
-        $('.filter-slider-frame').animate({
-          scrollLeft: '+=' + width
-        }, speed, function() {
-          // check arrows
-          checkArrows();
-        });
-      }
-    }); // end on arrow click
-    
-    $(window).on("load resize", function() {
-      checkArrows();
-      $('#filter-slider .filter-slider-item').each(function(i) {
-        var $this = $(this),
-          left = $this.width() * i;
-        $this.css({
-          left: left
-        })
-      }); // end each
-    }); // end window resize/load
-    
-    var resizeId;
-    $(window).resize(function() {
-        clearTimeout(resizeId);
-        resizeId = setTimeout(doneResizing, 500);
-    });
-    
-  })(jQuery); // end function
-});
-
-
-/*------------------Modal Script------------------*/
-// Get the modal
-var modal = document.getElementById("create-new");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
